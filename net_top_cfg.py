5b982f1055b1a473c3a221c0b197d74881a17c34
albertj2004 <191067099+albertj2004@users.noreply.github.com>
2025-03-21 17:43:57 +1100

    Create net_top_cfg.py

---
+++ net_top_cfg.py
@@ -0,0 +1,4 @@
+data_dir = 'D:\data'
+exclude_ips = ['0.0.0.0']
+dev_if_cols = ['device', 'ifName', 'ipaddress', 'netmask']
+link_cols = ['a', 'a_if', 'a_ip', 'a_mask', 'b', 'b_if', 'b_ip', 'b_mask']
