7f1ad3484e5cf8cceda9684d954381512eec57ad
albertj2004 <191067099+albertj2004@users.noreply.github.com>
2025-04-14 15:25:44 +1000

    Update f5_cfg.py

--- f5_cfg.py
+++ f5_cfg.py
@@ -1,26 +1,58 @@
-import os
-from datetime import datetime
+from common_cfg import *
+import common_utils
+from common_utils import *
 
-share_dir = f'D:\F5'
-data_dir=f'D:\F5'
-os.makedirs(data_dir, exist_ok=True)
+data_type = 'F5'
+sys_name = 'f5'
+log_file = mk_day_dir(source=data_type) + fr'\{sys_name}.log'
+common_utils.my_logger = my_logger = set_logger(file=log_file)
 
-f5_adm_list = """
-ltm_1
-ltm_2
-""".strip().split('\n')
-f5_adm_list = [h.strip() for h in f5_adm_list]
+cfg_file = f'{sys_name}.cfg.yaml'
+if not check_dirs(dirs=[job_dir + fr'\{cfg_file}']):
+    exit(0)
 
-broken_dev_list = """
-ltm_3
-""".strip().split('\n')
-broken_dev_list = [h.strip() for h in broken_dev_list]
+cfg_data = read_yaml_file(file=job_dir + fr'\{cfg_file}')
+f5_broken_list = [f5.strip() for f5 in cfg_data['f5_broken_list'].strip().split('\n')]
+f5_list = [f5.strip().split('.')[0] for f5 in cfg_data['f5_list'].strip().split('\n')]
+f5_list = sorted(set(f5_list))
+f5_working_list = [f5 for f5 in f5_list if f5 not in f5_broken_list]
+f5_working_list = sorted(set(f5_working_list))
 
+bigiq_obj_url_map = ltm_obj_url_map = {
+    'vs':  '/mgmt/tm/ltm/virtual',
+    'pool': '/mgmt/tm/ltm/pool',
+    'rule': '/mgmt/tm/ltm/rule',
+    'profile': '/mgmt/tm/ltm/profile',
+    'self': '/mgmt/tm/net/self',
+    'monitor': '/mgmt/tm/ltm/monitor',
+    'node': '/mgmt/tm/ltm/node',
+    'persistence': '/mgmt/tm/ltm/persistence',
+    'policy': '/mgmt/tm/ltm/policy',
+}
 
-set F5=gtm
-set USER=<>
-set PWD=<>
-curl -k -X POST "https://%F5%/mgmt/shared/authn/login" ^
--H "Content-Type: application/json" ^
--d "{\"username\":\"%USER%\",\"password\":\"%PWD%\",\"loginProviderName\":\"tmos\"}"
-"""
+ltm_obj_attr_map = {
+    'vs': ['profiles', 'policies'],
+    'pool': ['members'],
+}
+
+link_objs = ['persistence', 'profile', 'monitor']
+
+dns_type_list = ['a', 'aaaa', 'cname', 'mx', 'naptr', 'srv']
+
+gtm_obj_url_map = {
+    'dc':  '/mgmt/tm/gtm/datacenter',
+    'top':  '/mgmt/tm/gtm/topology',
+    'dns':  '/mgmt/tm/gtm/wideip',
+    'pool':  '/mgmt/tm/gtm/pool',
+    'prober_pool':  '/mgmt/tm/gtm/prober-pool',
+    'server':  '/mgmt/tm/gtm/server',
+    'listener':  '/mgmt/tm/gtm/listener',
+    'monitor':  '/mgmt/tm/gtm/monitor',
+}
+
+gtm_obj_attr_map = {
+    'server': ['devices', 'virtual-servers'],
+    'prober-pool': ['members'],
+}
+
+svc_cred_file = f'{sys_name}.cred'
