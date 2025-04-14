3ba4b7ea43b6292070381547d0ec36f8d3dad75f
albertj2004 <191067099+albertj2004@users.noreply.github.com>
2025-04-14 15:22:21 +1000

    Create common_cfg.py

---
+++ common_cfg.py
@@ -0,0 +1,9 @@
+cred_dir = fr'D:\data\cred'
+job_dir = fr'D:\data\job'
+python_exe = fr'"C:\Program Files\Python313\python.exe"'
+putty_exe = fr'"C:\Program Files\PuTTY\plink.exe"'
+job_code_map = {
+    'Faddom': 'faddom_api.py', 
+    'F5': 'f5_api.py'
+}
+
