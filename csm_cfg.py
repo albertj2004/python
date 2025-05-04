b45addb004bdbe1f1f05b25a835b99fc2d0957d0
albertj2004 <191067099+albertj2004@users.noreply.github.com>
2025-05-05 09:09:21 +1000

    Update csm_cfg.py

--- csm_cfg.py
+++ csm_cfg.py
@@ -3,11 +3,24 @@ import common_utils
 from common_utils import *
 
 data_type = 'Firewall'
-sys_name = 'csm'
-log_file = mk_day_dir(source=data_type) + fr'\{sys_name}.log'
+svc_name = 'csm'
+data_dir = mk_day_dir(source=data_type)
+log_file = data_dir + fr'\{svc_name}.log'
 common_utils.my_logger = my_logger = set_logger(file=log_file)
 
-csm = 'csm'
-dev_regex = 'fw'
-dev_exclude_regex = r'[_-](sys|admin|prd)(_|-|$)'
-svc_cred_file = f'{sys_name}.cred'
+csm = 'csmho'
+dev_regex = '.*'
+dev_exclude_regex = r'[_-](sys|admin)(_|-|$)'
+svc_cred_file = f'{svc_name}.cred'
+
+max_policy_rules = 500
+excel_cell_text_lmt = 32767
+policy_obj_types = [
+    'NetworkPolicyObject', 
+    'ServicePolicyObject', 
+    'PortListPolicyObject', 
+    'InterfaceRolePolicyObject',
+    'IdentityUserGroupPolicyObject',
+    'TimeRangePolicyObject',
+    'SecurityGroupPolicyObject',
+    ]
